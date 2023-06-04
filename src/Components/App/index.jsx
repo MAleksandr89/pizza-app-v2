33eda950860aa37d9f417e2c9ff730ee09ae6eb0
Makunin Aleksandr <amakunin@list.ru>
2023-06-04 09:39:26 +0300

    first commit

---
+++ src/Components/App/index.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return ( 
+        <div>
+            <h1>Hello Pizza-v2</h1>
+            <h2>Это новая пицца</h2>
+            <h3>Это старая пицца</h3>
+        </div>
+     );
+}
+ 
+export default App;
